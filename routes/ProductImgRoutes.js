f6e85a35a78fd328b44cb094c67ad46d0f458b11
Gion <69393137+gionsunday@users.noreply.github.com>
2023-09-20 07:09:14 +0100

    Update ProductImgRoutes.js

--- routes/ProductImgRoutes.js
+++ routes/ProductImgRoutes.js
@@ -10,6 +10,6 @@ const {getProductImage, uploadProductImage} = require("../controllers/productIma
 
 router.post('/uploadimage', upload.array('images'), uploadProductImage)
 router.get('/getimage/:uploadedBy', getProductImage)
-router.get('/getimage/:image_id', getProductImage)
+router.get('/getimage/:image_id', deleteProductImage)
 
 module.exports = router
