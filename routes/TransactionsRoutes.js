946d607323e9c498cb854c8b16db15a2a71a83f7
Gion <69393137+gionsunday@users.noreply.github.com>
2023-09-20 09:48:33 +0100

    Update TransactionsRoutes.js

--- routes/TransactionsRoutes.js
+++ routes/TransactionsRoutes.js
@@ -11,9 +11,9 @@ const {
 } = require('../controllers/transactions')
 
 router.route('/create').post(createTransaction)
-router.get('/getall/:business_id').post(getAllTransactions)
-router.get('/getsingle/:transaction_id').post(getSingleTransaction)
-router.route('/update/:transaction_id').post(updateTransaction)
+router.get('/getall/:business_id', getAllTransaction)
+router.get('/getsingle/:transaction_id', getSingleTransaction)
+router.post('/update/:transaction_id', updateTransaction)
 router.post('/delete/:transaction_id', deleteTransaction)
 
 module.exports = router
